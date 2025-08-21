98627122c249505c615e906b129b237636f4c77e
Anthony Becerra <antfoo1@users.noreply.github.com>
2025-08-20 23:28:12 -0700

    updated added color switch mode

--- Anthony/Ant.js
+++ Anthony/Ant.js
@@ -1,3 +1,23 @@
 function scrollToSection(id) {
     document.getElementById(id).scrollIntoView({ behavior: "smooth" });
 }
+
+const toggleBtn = document.getElementById("theme-toggle");
+const body = document.body;
+
+if (localStorage.getItem("theme") === "light") {
+    body.classList.add("light-mode");
+    toggleBtn.textContent = "🌃";
+}
+
+toggleBtn.addEventListener("click", () => {
+    body.classList.toggle("light-mode");
+
+    if (body.classList.contains("light-mode")) {
+        toggleBtn.textContent = "🌃"; 
+        localStorage.setItem("theme", "light");
+    } else {
+        toggleBtn.textContent = "🌄"; 
+        localStorage.setItem("theme", "dark");
+    }
+});
