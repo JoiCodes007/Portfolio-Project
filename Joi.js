12200d9e360f8dba35ea8d0032ee7b694c16b4f9
JOI MABREY <183445885+JoiCodes007@users.noreply.github.com>
2025-08-27 17:14:15 -0700

    moddified changes

--- Joi.js
+++ Joi.js
@@ -5,15 +5,15 @@ let x = 0;
 function toggleDarkMode(){
     const darkModeButton = document.getElementById('DarkMode');
     const caption = document.body;
+document.body.classList.toggle('dark-mode');
 
-
-    if (x ===0){
-        caption.style.color = '#430641ff'
-        x ++;
-    }else{
-        caption.style.color = '#25001fff'
-        x = 0
-    }
+    // if (x ===0){
+        // caption.style.color = '#160316ff'
+        // x ++;
+    // }else{
+        // caption.style.color = '#eae1e8ff'
+        // x = 0
+    // }
     if(darkModeButton.textContent === "Dark Mode"){
         darkModeButton.textContent = "Light Mode"
     }else{
